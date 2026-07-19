896ec71683fc31a238a13c6aa25bf10950d5380d
hatttri <57240541+hatttri@users.noreply.github.com>
2026-07-20 08:24:30 +0900

    最低限の Todo

---
+++ nuxt-app/app/pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const todos = ref<{id: number, text: string}[]>([{id: 1, text: '買い物'}, {id: 2, text: '掃除'}])
+
+function addTodo() {
+    const id = todos.value.reduce((a, b) => Math.max(a, b.id), 0) + 1
+    todos.value.push({id: id, text: ""})
+}
+
+function removeTodo(id: number) {
+    todos.value = todos.value.filter(todo => todo.id !== id)
+}
+</script>
+
+<template>
+    <button @click="addTodo">追加</button>
+    <div v-for="(todo, index) in todos" :key="todo.id">
+        <input type="text" v-model="todo.text" />
+        <button @click="removeTodo(todo.id)">削除</button>
+    </div>
+</template>
+
+<style scoped>
+</style>
